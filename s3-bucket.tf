51a0c8624d216c3f2bb218f5f75a610b72240f8e
Biswajit Pain <bpain2010@gamil.com>
2023-08-25 15:10:20 +0000

    update3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+ source  = "app.terraform.io/bpain-training/s3-bucket/aws"
 
   bucket_prefix = "my-s3-bucket-bpain"
   version = "2.8.0"
